c86d4c9a392d8eddace04e7072690de3bf9a5cd1
jokappo <kokerprim@gmail.com>
2025-06-09 07:25:21 +0000

    Add layout, page, and utility functions for mobile responsiveness and styling

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Next.js with Tailwind CSS!
+      </h1>
+
+      <Button type="primary"> premer</Button>
+    </div>
+    </>
+  );
+}
