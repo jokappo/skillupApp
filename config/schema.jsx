445c120ed76d550a57eb1da2b9966ab065694cb7
jokappo <kokerprim@gmail.com>
2025-06-15 23:23:00 +0000

    feat: add boolean and json types to schema and define courses table structure

--- config/schema.jsx
+++ config/schema.jsx
@@ -1,8 +1,28 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   subscriptionId: varchar({ length: 255 }),
+});
+
+//courses table
+export const coursesTable = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  cid: varchar({ length: 255 }).notNull().unique(),
+  name: varchar(),
+  description: varchar(),
+  noOfChapiters: integer().notNull(),
+  includeVideo: boolean().notNull().default(false),
+  bannerImageUrl: varchar().default(""),
+  level: varchar().notNull(),
+  category: varchar(),
+  couseJson: json(),
+  userEmail: varchar("userEmail")
+    .references(() => usersTable.email, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    })
+    .notNull(),
 });
