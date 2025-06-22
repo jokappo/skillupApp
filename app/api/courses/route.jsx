9506d81c6006c9b504115125c67fcc48a636b330
jokappo <kokerprim@gmail.com>
2025-06-22 23:54:46 +0000

    feat: enhance course fetching logic to include user authentication and handle missing course ID

--- app/api/courses/route.jsx
+++ app/api/courses/route.jsx
@@ -1,33 +1,75 @@
 import { db } from "@/config/db";
 import { coursesTable } from "@/config/schema";
-import { eq } from "drizzle-orm";
+import { currentUser } from "@clerk/nextjs/server";
+import { desc, eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-  const courseId = searchParams.get("courseId");
+  try {
+    const { searchParams } = new URL(req.url);
+    const courseId = searchParams.get("courseId");
+    const user = await currentUser();
 
-  if (!courseId) {
-    return new Response("Course ID is required", { status: 400 });
-  }
-
-  const result = await db
-    .select()
-    .from(coursesTable)
-    .where(eq(coursesTable.cid, courseId));
-    if (result.length === 0) {
+    if (!user) {
       return NextResponse.json({
-        message: "Course not found",
+        message: "User not authenticated",
         error: true,
-        success: false
-      })
+        success: false,
+      });
     }
-    console.log("courses details: ", result[0])
 
-  return NextResponse.json({
-    message: "Course details fetched successfully",
-    data: result[0],
-    success: true,
-    error: false
-  });
+    const email = user?.primaryEmailAddress?.emailAddress;
+
+    let result;
+
+    if (courseId) {
+      result = await db
+        .select()
+        .from(coursesTable)
+        .where(eq(coursesTable.cid, courseId));
+
+      if (result.length === 0) {
+        return NextResponse.json({
+          message: "Course not found",
+          error: true,
+          success: false,
+        });
+      }
+
+      return NextResponse.json({
+        message: "Course details fetched successfully",
+        data: result[0],
+        success: true,
+        error: false,
+      });
+    } else {
+      result = await db
+        .select()
+        .from(coursesTable)
+        .where(eq(coursesTable.userEmail, email))
+        .orderBy(desc(coursesTable.id));
+
+      if (result.length === 0) {
+        return NextResponse.json({
+          message: "No courses found for this user",
+          error: true,
+          success: false,
+        });
+      }
+
+      return NextResponse.json({
+        message: "Courses fetched successfully",
+        data: result,
+        success: true,
+        error: false,
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    return NextResponse.json({
+      message: "An error occurred while fetching courses",
+      error: true,
+      success: false,
+    });
+  }
 }
