4a970a520ba65853449d72cb364b106c04191226
jokappo <kokerprim@gmail.com>
2025-07-01 22:56:11 +0000

    feat: add ViewCourse component to display course details with viewCourse prop

---
+++ app/workspace/view-course/[courseId]/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import EditCourse from '../../edit-course/[courseId]/page'
+import { useParams } from 'next/navigation'
+
+function ViewCourse() {
+    const {courseId} = useParams()
+  return (
+    <div>
+      <EditCourse viewCourse={true}/>
+    </div>
+  )
+}
+
+export default ViewCourse
