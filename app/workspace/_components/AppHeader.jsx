115a4caa47ce47c2a72927e6684e80b1caad7132
jokappo <kokerprim@gmail.com>
2025-07-07 13:37:30 +0000

    feat: make AppHeader sticky for improved navigation

--- app/workspace/_components/AppHeader.jsx
+++ app/workspace/_components/AppHeader.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 function AppHeader({hideSideBare}) {
   return (
-    <div className='flex justify-between items-center p-4 bg-white shadow-md'>
+    <div className='flex justify-between items-center p-4 bg-white shadow-md sticky top-0 z-50'>
       {!hideSideBare && <SidebarTrigger/>}
       <UserButton></UserButton>
     </div>
